0199bf99c3112cd074860d2e42899be58553c62d
Jamee Kim <jameekim@nodaplife.me>
2021-06-08 18:13:23 -0400

    Add db schema and model for project entries

---
+++ server/models/projects.ts
@@ -0,0 +1,45 @@
+/**
+ * server/models/projects.ts
+ *
+ * Schema for project model shown on projects page
+ *
+ * Dohyun Kim 301058465
+ * Jun. 8, 2021
+ */
+
+import mongoose, { Schema } from "mongoose";
+
+const ProjectSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        collaborationType: String,
+        description: [String],
+        imgPath: {
+            type: String,
+            required: true,
+        },
+        imgAlt: {
+            type: String,
+            default: "An image of the project",
+        },
+        links: [{
+            name: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            },
+        }],
+    },
+    {
+        collection: "projects",
+    },
+);
+
+const Model = mongoose.model("Project", ProjectSchema);
+export default Model;
